080a16c1873fb3ce25d0a83de3978d201ef68aee
natsus <sample@example.com>
2014-11-20 00:06:56 +0900

    Key Generation, Signing, Verification の実装

--- agg_sig.h
+++ agg_sig.h
@@ -14,55 +14,147 @@ using namespace std;
 class AggSig {
   public:
     AggSig();
-    void point_print(const char* , const EC_POINT );
-/*
-    set_message();
-    key_gen();
-    sign();
-    verify();
-*/
+    ~AggSig();
+    void key_gen();
+    void sign( const char* , size_t );
+    bool vrfy(const char* , size_t );
 
   private:
     EC_PAIRING prg;
-    EC_POINT G1, G2, g2;
-    mpz_t p;
-    void get_g2(EC_POINT);
+    EC_POINT G1, G2, g2, v, h, s;
+    Field f ;
+    Element x;
+    mpz_t tmp;
+
+    void get_g2();
+    void point_print( const char* , const EC_POINT );
+    void element_print( const char* , const Element );
+    void set_mpz_from_element( mpz_t , Element );
 
 };
 
-AggSig::AggSig(){
+AggSig::AggSig()
+{
+  mpz_init(tmp);
 
   pairing_init(prg, "ECBN254");
   point_init(G1, prg->g1);
   point_init(G2, prg->g2);
   point_init(g2, prg->g2);
+  point_init(v , prg->g2);
+  point_init(h , prg->g1);
+  point_init(s , prg->g1);
 
-  get_g2(g2);
+  get_g2();
 
-  mpz_init(p);
-  mpz_set(p, prg->g1->order);
+  field_init(f, "bn254_fp");
+  // gmp_printf ("%s is an mpz %Zd\n", "order of f", f->order);
+  element_init(x, f);
+
+  key_gen();
 
 }
 
-void AggSig::get_g2(EC_POINT g2){
+AggSig::~AggSig()
+{
+  element_clear(x);
+
+  field_clear(f);
+
+  point_clear(G1);
+  point_clear(G2);
+  point_clear(g2);
+  point_clear(v);
+  point_clear(h);
+  point_clear(s);
+
+  pairing_clear(prg);
+
+  mpz_clear(tmp);
+}
+
+// Key Generation
+void AggSig::key_gen()
+{
+  // *** generation of secret key 'x' (random value) ***
+  element_random(x);
+  // gmp_printf ("%s is an mpz %Zd\n", "order of x", x->field->order);
+  // element_print("value of x: ", x);
+
+  // *** generation of public key 'v' ***
+  // ?? v = g_2^x ⇔ v = x g_2
+  set_mpz_from_element(tmp, x); // tmp(mpz_t) ← x(Element)
+  point_mul(v, tmp, g2);  // v ← g_2^x
+}
+
+// Signing
+void AggSig::sign( const char* m, size_t slen)
+{
+  // *** h ← H(M) ***
+  point_map_to_point(h, m, slen, 128);
+
+  // *** generation of signature 's' ***
+  set_mpz_from_element(tmp, x); // tmp(mpz_t) ← x(Element)
+  point_mul(s, tmp, h); // s ← h^x
+}
+
+// Verification
+bool AggSig::vrfy(const char* m, size_t slen)
+{
+  // *** initialization ***
+  Element t1, t2;
+  element_init(t1, prg->g3);
+  element_init(t2, prg->g3);
+  bool rslt = false;
+
+
+  // *** h ← H(M) ***
+  point_map_to_point(h, m, slen, 128);
+
+
+  // *** pairing computation ***
+  // *** e(s,g2), e(h,v)     ***
+  pairing_map(t1, s, g2, prg);
+  pairing_map(t2, h, v,  prg);
+  rslt = element_cmp(t1, t2) == 0 ? true : false;
+
+  // *** finalization ***
+  element_clear(t1);
+  element_clear(t2);
+
+  return rslt;
+}
+
+void AggSig::get_g2()
+{
   // init
-  mpz_t cofactor;
-  mpz_init(cofactor);
-  mpz_set(cofactor, prg->g2->cofactor);
-  // gmp_printf ("%s is an mpz %Zd\n", "cofactor", cofactor);
+  mpz_set(tmp, prg->g2->cofactor);
+  // gmp_printf ("%s is an mpz %Zd\n", "cofactor", tmp);
   EC_POINT P;
   point_init(P, prg->g2);
 
   do {
     point_random(P);
-    point_mul(g2, cofactor, P);
+    point_mul(g2, tmp, P);
     // cout << point_is_infinity(g2) << endl;
   } while (point_is_infinity(g2) != 0) ;   // exit not infinity
   // point_print("g2", g2);
 
   // clear
-  mpz_clear(cofactor);
   point_clear(P);
+}
+
+void  AggSig::set_mpz_from_element(mpz_t tmp, Element x)
+{
+  int size = element_get_str_length(x);
+  char *str = new char[size];
+
+  element_get_str(str, x);
+
+  mpz_set_str(tmp, str, 16);
+  // gmp_printf ("%s is an mpz %Zx\n", "value of x", tmp);
+
+  delete [] str;
 }
 
 void AggSig::point_print(const char *s, const EC_POINT x)
@@ -74,4 +166,15 @@ void AggSig::point_print(const char *s, const EC_POINT x)
   printf("%s: %s\n", s, str);
 
   free(str);
+}
+
+void AggSig::element_print(const char *s, const Element x)
+{
+  int size = element_get_str_length(x);
+  char *str = new char[size];
+
+  element_get_str(str, x);
+  cout << s << str << endl;
+
+  delete [] str;
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
+#undef NDEBUG
 #include <iostream>
+#include <string.h>
+#include <assert.h>
+
 #include "agg_sig.h"
 
-int main() {
+int main()
+{
   AggSig hoge;
+  char* m = "hogehoge";
+  bool r1, r2;
+
+  hoge.sign(m, strlen(m));
+  r1 = hoge.vrfy(m, strlen(m));
+  r2 = hoge.vrfy("hogehogf", strlen("hogehogf"));
+  assert( true  == r1 );
+  assert( false == r2 );
 
   return 0;
 }
